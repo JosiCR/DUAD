f8c4f29c7d10e90ff2efb61ba6f06e96b4517ac0
Josias Nuñez <JosiCR.@email.com>
2026-04-24 16:24:02 -0600

    javaScript

---
+++ Modulo 2/JavaScript/ejercicio1.js
@@ -0,0 +1,5 @@
+const list = [10, 20, 30, 40];
+
+for (let i =0; i < list.length; i++) {
+    console.log(list[i]);
+}
---
+++ Modulo 2/JavaScript/ejercicio3.js
@@ -0,0 +1,7 @@
+const temp = [0, 10, 20, 40, 60];
+
+const result = temp.map((temp) => {
+    return (temp * 9/5) + 32;
+});
+
+console.log(result)
