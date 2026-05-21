a355f8cded16c5e61cedf75cadb0f66010139165
Josias Nuñez <JosiCR.@email.com>
2026-05-21 13:21:54 -0600

    async y promises

---
+++ Modulo 2/JavaScript/Promises/ejercicio4.js
@@ -0,0 +1,31 @@
+fetch("https://reqres.in/api/users/23" , {
+    headers: {
+        "x-api-key":"free_user_3DuUjp72jxMDzc1XzNsX2rjcPsQ"
+
+    }
+})
+    .then(function(response){
+
+        if(!response.ok){
+            throw new Error("User not found");
+        }
+
+        return response.json();
+
+    })
+
+    .then(function(data){
+
+        console.log(data.data.first_name, data.data.last_name, data.data.email);
+
+    })
+
+    .catch(function(error){
+        console.log("User not found");
+
+    })
+
+    .finally(function(){
+        console.log("Request finished")
+    })
+    
--- Modulo 2/JavaScript/ejercicio1.js
+++ Modulo 2/JavaScript/ejercicio1.js
@@ -3,3 +3,6 @@ const list = [10, 20, 30, 40];
 for (let i =0; i < list.length; i++) {
     console.log(list[i]);
 }
+
+
+
